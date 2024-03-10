ac0f9b259d81c5892535922853b29a88d6140397
Yumin Cho <dbals081216@gmail.com>
2024-03-11 02:11:14 +0900

    :sparkles: impl `Divider`

---
+++ src/components/widget/Divider.tsx
@@ -0,0 +1,15 @@
+const Divider = () => {
+  return (
+    <hr
+      style={{
+        width: "100%",
+        height: "1px",
+        backgroundColor: "#ddd",
+        border: "none",
+        margin: "0",
+      }}
+    />
+  );
+};
+
+export default Divider;
